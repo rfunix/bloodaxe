d5f6609d4d146277efdde20e59e5f5f6c78316f4
rfunix <rafinha.unix@gmail.com>
2020-03-13 00:12:28 -0300

    Add show_metrics

--- bloodaxe.py
+++ bloodaxe.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+import statistics
 import time
 from dataclasses import dataclass
 from multiprocessing import Pool
@@ -11,6 +12,7 @@ import typer
 from httpx._exceptions import NetworkError
 from httpx.exceptions import HTTPError
 from jinja2 import Template
+from tabulate import tabulate
 
 HTTP_METHODS_FUNC_MAPPING = {"GET": "make_get_request", "POST": "make_post_request"}
 SUCCESS = typer.style("success", fg=typer.colors.GREEN, bold=True)
@@ -18,6 +20,15 @@ ERROR = typer.style("error", fg=typer.colors.RED, bold=True)
 
 REQUEST_MESSAGE = "Request {}, name={}, url={}"
 START_MESSAGE = "Start bloodaxe, number_of_process={}, duration={} seconds"
+
+TABLE_HEADERS = [
+    "Total success flows",
+    "Total error flows",
+    "Total flows",
+    "Mean time",
+    "Standard deviation",
+    "Total time",
+]
 
 
 class FlowError(Exception):
@@ -115,11 +126,32 @@ def execute_flow(toml_data):
     return result
 
 
-def show_metrics(flows):
-    success = len([flow for flow in flows if flow.success])
-    errors = len([flow for flow in flows if flow.error])
+def generate_mean_time(flows):
+    total_time = sum([flow.duration for flow in flows])
 
-    typer.echo(f"Total flows={len(flows)}, success={success}, errors={errors}")
+    return total_time / len(flows)
+
+
+def show_metrics(flows, total_time):
+    success_flows = [flow for flow in flows if flow.success]
+    error_flows = [flow for flow in flows if flow.error]
+
+    mean_time = generate_mean_time(success_flows)
+    standard_deviation = statistics.stdev([flow.duration for flow in success_flows])
+
+    seconds_mask = "{0:.2f} seconds"
+
+    row = [
+        len(success_flows),
+        len(error_flows),
+        len(flows),
+        seconds_mask.format(round(mean_time, 2)),
+        seconds_mask.format(round(standard_deviation, 2)),
+        seconds_mask.format(round(total_time, 2)),
+    ]
+
+    typer.echo("\n")
+    typer.echo(tabulate([row], headers=TABLE_HEADERS))
 
 
 def start(toml_data):
@@ -130,6 +162,7 @@ def start(toml_data):
     typer.secho(
         START_MESSAGE.format(number_of_process, duration), fg=typer.colors.CYAN, underline=True, bold=True
     )
+    typer.echo("\n")
 
     start_time = time.time()
     while True:
@@ -141,7 +174,7 @@ def start(toml_data):
         with Pool(processes=number_of_process) as p:
             flows += tuple(p.map(execute_flow, [toml_data for _ in range(number_of_process)]))
 
-    show_metrics(flows)
+    show_metrics(flows, elapsed_seconds)
 
 
 def bloodaxe(config_file: Path):
